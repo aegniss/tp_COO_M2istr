a051a7904290a5e50d39b9082ac49ae7766f4f6b
Nassim Messaadi <msn2738a@u1-105-06d.univ-tlse3.fr>
2023-09-27 17:31:26 +0200

    save

--- brewsim/high_level/models.py
+++ brewsim/high_level/models.py
@@ -4,9 +4,13 @@ from django.db import models
 class Departement (models.Model):
     numero=models.IntegerField()
     prix_m2=models.IntegerField()
+    def __str__(self):
+        return self.Departement
 
 class Ingredient (models.Model):
     nom=models.CharField(max_length=100)
+        def __str__(self):
+            return self.nom
 
 class Prix (models.Model):
     ingredient =models.ForeignKey(
@@ -15,6 +19,8 @@ class Prix (models.Model):
         # blank=True, null=True,
         # related_name="+",
         )
+    def __str__(self):
+        return self.ingredient
 
     departement =models.ForeignKey(
         Departement,
@@ -22,23 +28,37 @@ class Prix (models.Model):
         # blank=True, null=True,
         # related_name="+",
         )
+    def __str__(self):
+        return self.departement
 
     prix=models.IntegerField()
 
+    def __str__(self):
+        return self.prix
 
 class QuantiteIngredient (models.Model):
     ingredient =models.ForeignKey(
         Ingredient,
         on_delete=models.PROTECT,
-        # blank=True, null=True,
+        blank=True, null=True,
         # related_name="+",
         )
+    def __str__(self):
+        return self.ingredient
 
     quantite =models.IntegerField()
+        def __str__(self):
+            return self.quantite
+
 
 class Machine(models.Model):
     nom=models.CharField(max_length=100)
+        def __str__(self):
+            return self.nom
+
     prix=models.IntegerField()
+        def __str__(self):
+            return self.prix
 
 
 
@@ -49,12 +69,20 @@ class Action(models.Model):
         # blank=True, null=True,
         # related_name="+",
         )
+            def __str__(self):
+                return self.machine
 
     commande=models.CharField(max_length=100)
+            def __str__(self):
+                return self.commande
 
     duree=models.IntegerField()
+            def __str__(self):
+                return self.duree
 
     ingredients = models.ManyToManyField(QuantiteIngredient)
+                def __str__(self):
+                    return self.ingredients
 
     action=models.ForeignKey(
         "self",
@@ -63,17 +91,22 @@ class Action(models.Model):
          related_name="+",
         )
 
-
+        def __str__(self):
+            return self.action
 
 class Recette (models.Model):
     nom=models.CharField(max_length=100)
+                def __str__(self):
+                    return self.nom
+
     action =models.ForeignKey(
         Action,
         on_delete=models.PROTECT,
         # blank=True, null=True,
         # related_name="+",
         )
-
+                def __str__(self):
+                    return self.action
 
 
 
@@ -85,11 +118,21 @@ class Usine (models.Model):
         # blank=True, null=True,
         # related_name="+",
         )
+            def __str__(self):
+                    return self.departement
 
     taille=models.IntegerField()
+                    def __str__(self):
+                        return self.taille
 
     machines=models.ManyToManyField(Machine)
+                def __str__(self):
+                    return self.machines
 
     recettes=models.ManyToManyField(Recette)
+                def __str__(self):
+                    return self.recettes
 
     stocks=models.ManyToManyField(QuantiteIngredient)
+                def __str__(self):
+                    return self.stocks
