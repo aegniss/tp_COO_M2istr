15741cae46e5a51c530ef572240819a504c41346
aegniss <messaadinassim@gmail.com>
2023-12-26 17:15:18 +0100

    home

--- brewsim/high_level/brewsim/brewsim/urls.py
+++ brewsim/high_level/brewsim/brewsim/urls.py
@@ -16,7 +16,53 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from high_level import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path(
+        "Departement/<int:pk>",
+        views.DepartementDetailView.as_view(),
+        name="Departement",
+    ),
+    path(
+        "Recette/<int:pk>",
+        views.RecetteDetailView.as_view(),
+        name="Recette",
+    ),
+    path(
+        "Ingredient/<int:pk>",
+        views.IngredientDetailView.as_view(),
+        name="Ingredient",
+    ),
+    path(
+        "Machine/<int:pk>",
+        views.MachineDetailView.as_view(),
+        name="Machine",
+    ),
+    path(
+        "QuantiteIngredient/<int:pk>",
+        views.QuantiteIngredientDetailView.as_view(),
+        name="QuantiteIngredient",
+    ),
+    path(
+        "Usine/<int:pk>",
+        views.UsineDetailView.as_view(),
+        name="Usine",
+    ),
+    path(
+        "Action/<int:pk>",
+        views.ActionDetailView.as_view(),
+        name="Action",
+    ),
+    path(
+        "Prix/<int:pk>",
+        views.PrixDetailView.as_view(),
+        name="Prix",
+    ),
+    path(
+        "Departement/api/<int:pk>",
+        views.DepartementDetailAPIView.as_view(),
+        name="Departement_api",
+    ),
 ]
